dbb097b285b79e023c80a9c4285cdb7f7e2782d5
iamdwn <truongdinhdongduong@gmail.com>
2024-08-14 23:58:59 +0700

    feat: upload ggdrive

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using Path = System.IO.Path;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string[] Scopes = { DriveService.Scope.DriveFile };
+        static string ApplicationName = "Google Drive API WPF App";
+        string filePath;
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+                txtEditor.Text = File.ReadAllText(filePath);
+                txtPath.Text = filePath;
+            }
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                MessageBox.Show("Uploaded successfully !!");
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+                UploadFile(service, filePath);
+        }
+
+        static void UploadFile(DriveService service, string filePath)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = Path.GetFileName(filePath)
+            };
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                request = service.Files.Create(fileMetadata, stream, "application/octet-stream");
+                request.Fields = "id";
+                request.Upload();
+            }
+            var file = request.ResponseBody;        }
+    }
+}
